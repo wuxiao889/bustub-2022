e5dce7b74028090758d131fc1f6a2d80653489a1
wuxiao1998 <917660818@qq.com>
2022-11-28 14:33:24 +0800

    add scale_test

---
+++ test/storage/b_plus_tree_scale_test.cpp
@@ -0,0 +1,80 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// b_plus_tree_insert_test.cpp
+//
+// Identification: test/storage/b_plus_tree_insert_test.cpp
+//
+// Copyright (c) 2015-2021, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <random>
+#include <string>
+
+#include "buffer/buffer_pool_manager_instance.h"
+#include "gtest/gtest.h"
+#include "storage/index/b_plus_tree.h"
+#include "test_util.h"  // NOLINT
+
+namespace bustub {
+
+TEST(BPlusTreeTests, InsertTest1) {
+  // create KeyComparator and index schema
+  auto key_schema = ParseCreateStatement("a bigint");
+  GenericComparator<8> comparator(key_schema.get());
+
+  auto *disk_manager = new DiskManager("test.db");
+  BufferPoolManager *bpm = new BufferPoolManagerInstance(50, disk_manager);
+  // create b+ tree
+  BPlusTree<GenericKey<8>, RID, GenericComparator<8>> tree("foo_pk", bpm, comparator, 3, 3);
+  GenericKey<8> index_key;
+  RID rid;
+  // create transaction
+  auto *transaction = new Transaction(0);
+
+  // create and fetch header_page
+  page_id_t page_id;
+  auto header_page = bpm->NewPage(&page_id);
+  (void)header_page;
+
+  int size = 50;
+  std::vector<int64_t> keys(size);
+  std::iota(keys.begin(), keys.end(), 1);
+  std::random_device rd;
+  std::mt19937 g(rd());
+  std::shuffle(keys.begin(), keys.end(), g);
+
+  for (auto key : keys) {
+    int64_t value = key & 0xFFFFFFFF;
+    rid.Set(static_cast<int32_t>(key >> 32), value);
+    index_key.SetFromInteger(key);
+    tree.Insert(index_key, rid, transaction);
+  }
+
+  tree.Draw(bpm, "/home/wxx/bustub-private/build-vscode/bin/ScaleTest_" + std::to_string(size) + ".dot");
+
+  std::vector<RID> rids;
+  for (auto key : keys) {
+    rids.clear();
+    index_key.SetFromInteger(key);
+    tree.GetValue(index_key, &rids);
+    EXPECT_EQ(rids.size(), 1);
+
+    int64_t value = key & 0xFFFFFFFF;
+    EXPECT_EQ(rids[0].GetSlotNum(), value);
+  }
+
+  bpm->UnpinPage(HEADER_PAGE_ID, true);
+  delete transaction;
+  delete disk_manager;
+  delete bpm;
+  remove("test.db");
+  remove("test.log");
+}
+
+}  // namespace bustub
